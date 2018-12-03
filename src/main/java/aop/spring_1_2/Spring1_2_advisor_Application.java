76b6bf840c1f82db674bff1c420462a5b7056479
haobin <815754194@qq.com>
2018-12-03 22:23:35 +0800

    advisor 与 advice

--- src/main/java/aop/spring_1_2/Spring1_2_advisor_Application.java
+++ src/main/java/aop/spring_1_2/Spring1_2_advisor_Application.java
@@ -13,8 +13,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @author HaoBin
  * @version $Id: Spring1_2_Application.java, v0.1 2018/11/29 21:20 HaoBin 
  */
-public class Spring1_2_Application {
+public class Spring1_2_advisor_Application {
 
+    /**
+     * 只拦截特定的方法
+     * @param args
+     */
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring_1_2_advice.xml");
         // 获取AOP代理 UserServiceProxy
