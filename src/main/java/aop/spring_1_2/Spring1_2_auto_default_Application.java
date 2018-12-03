9bfe92d388309d8acd2f6b81f5b662b1000ff3a9
haobin <815754194@qq.com>
2018-12-03 22:55:19 +0800

    defaultAutoAdvisorProxy

--- src/main/java/aop/spring_1_2/Spring1_2_auto_default_Application.java
+++ src/main/java/aop/spring_1_2/Spring1_2_auto_default_Application.java
@@ -14,14 +14,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @author HaoBin
  * @version $Id: Spring1_2_Application.java, v0.1 2018/11/29 21:20 HaoBin 
  */
-public class Spring1_2_auto_beanname_Application {
+public class Spring1_2_auto_default_Application {
 
     /**
-     * 自动生成代理的bean
-     * @param args
+     * 自动匹配advisor， 根据正则表达式
      */
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring_1_2_BeanNameAutoProxy.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring_1_2_DefaultAdvisorAutoProxy.xml");
         // 获取AOP代理 UserServiceProxy
         UserService userService = (UserService) context.getBean(UserService.class);
         OrderService orderService = (OrderService) context.getBean(OrderService.class);
